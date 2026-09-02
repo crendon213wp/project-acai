c3f0e0ea24b8000ed0c43ecfb8ea95c675cb37cc
Cisco Rendon <ciscorendon213@gmail.com>
2026-09-02 16:15:09 -0400

    Create site.config.js

---
+++ site.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    site: {
+        title: 'Project Title',
+        url: 'https://example.com',
+        author: 'Your Name',
+        description: 'Project Description',
+        language: 'en',
+        locale: 'en_US'
+    },
+
+    seo: {
+        changefreq: 'weekly',
+        priority: 1.0
+    }
+};
